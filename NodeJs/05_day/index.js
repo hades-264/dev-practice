13432c8a26f44a58362f914482c53504d6386300
Hritik Dethaliya <hades-264@users.noreply.github.com>
2025-03-12 20:04:51 +0530

    Handling url

---
+++ NodeJs/05_day/index.js
@@ -0,0 +1,33 @@
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+
+const myServer = http.createServer((req, res) => {
+    if (req.url === '/favicon.ico') {
+       return res.end();
+    }
+    // console.log(req.headers);
+    const log = `${Date.now()}: ${req.url} New request received\n`;
+    const myUrl = url.parse(req.url, true);
+    console.log(myUrl);
+
+    // console.log(myUrl.pathname);
+    
+    fs.appendFile('./log.txt', log, (err, data) => {
+        switch (myUrl.pathname) {
+            case '/': 
+                res.end("HomePage");
+                break;
+            case '/about': 
+                const username =  myUrl.query.myname;
+                res.end(`Hi, ${username}`);
+                break;
+            default: 
+                res.end("404 Not Found");
+        }
+    });
+});
+
+myServer.listen(8000, () => console.log("Server Started!"));
+
+
