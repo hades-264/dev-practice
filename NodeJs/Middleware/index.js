37f39f4091d1e80b9ceb82e9cb54854a2c3e60d4
Hritik Dethaliya <hades-264@users.noreply.github.com>
2025-03-19 08:19:35 +0530

    Middleware

---
+++ NodeJs/Middleware/index.js
@@ -0,0 +1,106 @@
+const express = require('express');
+const fs = require('fs');
+const users = require('./MOCK_DATA.json');
+
+const app = express();
+const port = 8000;
+
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+    // console.log('Hello from middleware 1');
+    // req.myName = 'Tony';
+    // return res.json({ msg: 'This request is blocked by middleware 1'});
+    fs.appendFile('./log.txt', `${Date.now()}: ${req.method} ${req.path}\n`, 
+    (err, data) => {
+        next();
+    })
+});
+
+/* app.use((req, res, next) => {
+    console.log('Hello from middleware 2', req.myName);
+    // return res.end('Hey');
+    next();
+}); */ 
+
+// app.get('/users', (req, res) => {
+//     return res.json(users);
+// });
+
+app.get('/users', (req, res) => {
+    const html =
+        `<ul>
+            ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+        </ul>`;
+    res.send(html);
+});
+
+app.get('/api/users', (req, res) => {
+    // console.log('Response from GET route', req.myName);
+    // res.setHeader('myName', 'Tony');
+    res.setHeader('X-myName', 'Tony'); // Custom Header
+    console.log(req.headers);
+    return res.json(users);
+});
+
+app.route('/api/users/:id')
+    .get((req, res) => {
+        const id = Number(req.params.id);
+        const user = users.find((user) => user.id === id);
+        if (user === null || user === undefined) {
+            return res.json("No such user");
+        }
+        else
+            return res.json(user);
+    })
+    .patch((req, res) => {
+        // Edit user with id
+        const id = Number(req.params.id);
+        const index = users.findIndex((user) => user.id === id);
+        users[index].email = `${users[index].first_name}.${users[index].last_name}@gmail.com`;
+        fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) => {
+            return res.json({ status: 'Success' });
+        });
+    })
+    .delete((req, res) => {
+        // Delete user with id
+        const id = Number(req.params.id);
+        // const user = users.find((user) => user.id === id);
+        const index = users.findIndex((user) => user.id === id);
+        users.splice(index, 1);
+        fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) => {
+            return res.json({ status: 'Success', userid: id });
+        });
+    });
+
+app.post('/api/users/', (req, res) => {
+    // Create new user
+    const body = req.body;
+    users.push({ id: users.length + 1, ...body });
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) => {
+        return res.json({ status: 'Success', id: users.length });
+    });
+    console.log("Body", body);
+});
+
+/* app.get('/api/users/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+});
+
+app.patch('/api/users/:id', (req, res) => {
+    // Edit user with id
+    return res.json({ status: 'Pending'});
+});
+
+app.delete('/api/users/:id', (req, res) => {
+    // Delete user with id
+    return res.json({ status: 'Pending'});
+}); */
+
+
+app.listen(port, () => {
+    console.log(`Server Started at port: ${port}`);
+})
